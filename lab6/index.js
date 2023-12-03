dc93f011e87505de13c02fea9c5744aeefd271ef
Julia Kućmierczyk <89534632+JuliaKucmierczyk@users.noreply.github.com>
2023-12-03 12:30:51 +0100

    Add files via upload

--- lab6/index.js
+++ lab6/index.js
@@ -1,12 +1,73 @@
-window.addEventListener("deviceorientation", onDeviceMove);
+document.addEventListener("DOMContentLoaded", () => {
+  const ball = document.querySelector(".ball");
+  const hole = document.querySelector(".hole");
+  const timerSpan = document.querySelector("#time");
 
-function onDeviceMove(event) {
-  console.log(event);
-}
+  let timer = 0;
+  let score = 0;
 
-function animate() {
-  console.log(Date.now());
-  requestAnimationFrame(animate);
-}
+  window.addEventListener("deviceorientation", handleOrientation);
 
-requestAnimationFrame(animate);
+  function handleOrientation(event) {
+    const beta = event.beta * 2;
+    const gamma = event.gamma * 2;
+
+    const newX = (gamma / 90) * 150;
+    const newY = (beta / 90) * 150;
+
+    ball.style.transform = `translate(-50%, -50%) translate(${newX}px, ${newY}px)`;
+
+    checkCollision();
+  }
+
+  function checkCollision() {
+    const ballRect = ball.getBoundingClientRect();
+    const holeRect = hole.getBoundingClientRect();
+
+    if (
+      ballRect.top >= holeRect.top &&
+      ballRect.bottom <= holeRect.bottom &&
+      ballRect.left >= holeRect.left &&
+      ballRect.right <= holeRect.right
+    ) {
+      score++;
+      resetGame();
+    }
+  }
+
+  function resetGame() {
+    placeHoleRandomly();
+
+    document.querySelector("#points").innerText = score;
+
+    ball.style.transform = "translate(-50%, -50%) translate(0, 0)";
+  }
+
+  function placeHoleRandomly() {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const holeSize = 50;
+    const maxX = windowWidth - holeSize;
+    const maxY = windowHeight - holeSize;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    hole.style.left = `${randomX}px`;
+    hole.style.top = `${randomY}px`;
+  }
+
+  setInterval(() => {
+    timer++;
+    timerSpan.innerText = `${timer}s`;
+
+    if (timer === 60) {
+      alert(`Game Over! Your score is ${score}`);
+      timer = 0;
+      resetGame();
+    }
+  }, 1000);
+
+  placeHoleRandomly();
+});
